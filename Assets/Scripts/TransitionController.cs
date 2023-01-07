a6331d75dd7ca242eee1e667c0629d50f5779a17
kirchner-trevor <kirchner.trevor@gmail.com>
2023-01-06 21:35:18 -0600

    Added transition controller.

---
+++ Assets/Scripts/TransitionController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionController : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void GoToTitle()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void GoToLevelSelect()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GoToLevelInfo()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void GoToGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void GoToEndLevel()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
