eb793b2679c3035d54a0726ccfbecb42c7c203a8
kirchner-trevor <kirchner.trevor@gmail.com>
2023-01-08 21:06:01 -0600

    Display awarded point in space of plant.

--- Assets/Scripts/ScoreChangeController.cs
+++ Assets/Scripts/ScoreChangeController.cs
@@ -13,6 +13,12 @@ public class ScoreChangeController : MonoBehaviour
     {
         StartCoroutine(ShowUp(score));
     }
+
+    public void ShowScore(GameGridSpace gameGridSpace)
+    {
+        StartCoroutine(ShowUp(gameGridSpace));
+    }
+
     IEnumerator ShowUp(int score)
     {
         float randTime = Random.Range(0f, 1f);
@@ -22,6 +28,16 @@ public class ScoreChangeController : MonoBehaviour
 
         PlaceScore();
     }
+
+    IEnumerator ShowUp(GameGridSpace gameGridSpace)
+    {
+        float randTime = Random.Range(0f, 0.5f);
+        yield return new WaitForSeconds(randTime);
+        scoreObject = Instantiate(scorePrefab);
+        scoreObject.GetComponentInChildren<TMPro.TMP_Text>().text = gameGridSpace.GetMostRecentPlantData().Plant.PointsForFullyGrown.ToString();
+        scoreObject.transform.SetParent(gameGridSpace.gameObject.transform, false);
+    }
+
     public void PlaceScore()
     {
         float randSpot1 = Random.Range(400,900);
